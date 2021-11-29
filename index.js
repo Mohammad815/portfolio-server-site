f10f82dfdb275e3532b2d9ee95fc66dcc1b959d9
Mohammad815 <mohammadforhad176@gmail.com>
2021-11-29 17:07:41 +0600

    update

--- index.js
+++ index.js
@@ -2,10 +2,35 @@ const express = require('express')
 const cors = require("cors")
 
 const app = express()
-app.use(cors())
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
+
 const port = process.env.PORT || 4000
 
+app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ns6st.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+  try{
+      await client.connect();
+      console.log('database mia connected  successfuflly')
+  }
+  finally{
+      // await client.close();
+  }
+}
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/cv', (req, res) => {
   res.download('Forhad.pdf')
 })
 
